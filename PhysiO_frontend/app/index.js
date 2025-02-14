cabe10986087c6a48f4d7bafe4f7aee90d290395
Tala-Al-Dibs <talad6791@gmail.com>
2025-02-14 22:14:44 +0200

    initial set up of the project

---
+++ PhysiO_frontend/app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View className="pt-20 bg-red-200">
+      <Text className="text-3xl text-center">Home</Text>
+    </View>
+  )
+}
