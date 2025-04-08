cc4d4275b4d393a454287927004ddf6c7e69d844
zainab1AT <engzainabatwa1@gmail.com>
2025-04-08 11:47:20 +0300

    finish everything related to profile

--- constants/apiConfig.ts
+++ constants/apiConfig.ts
@@ -1,9 +1,8 @@
-export const SPRINGPORT8080 = "https://82f5-37-60-146-110.ngrok-free.app";
+export const SPRINGPORT8080 = "http://192.168.1.20:8080";
 export const FASTAPIPORT8000 = "https://a9af-84-242-56-27.ngrok-free.app";
 export const USERID = 1;
 export const TOKEN =
-
-  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJub3VyMiIsImlhdCI6MTc0Mzc3Mjc1MCwiZXhwIjoxNzQzODU5MTUwfQ.lxwQydNlHeBFfrb4Jc30BQrgmlsrZCjiOTvnYAPsyPw";
+"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJUZXN0VXNlciIsImlhdCI6MTc0NDEwMTIyNiwiZXhwIjoxNzQ0MTg3NjI2fQ.Eh8Ob0kigaTwiJk28i9Rl4PPdRs5j6spCQ6-h1f6O9I";
 
 // https://0e06-94-73-8-143.ngrok-free.app -> http://localhost:8080
 // https://f6cc-94-73-8-143.ngrok-free.app -> http://localhost:8000
